d6919164de2c734ccd15787ea84ad8ff2c10ecc2
Pritam Mandal <pritampmandal@gmail.com>
2025-06-07 20:27:59 +0530

    done some improvements at card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Card(props) {
     return (
         <>
-            <div className='w-80 h-50 text-black rounded-2xl bg-cover bg-center relative flex flex-col justify-end p-5 gap-3'
+            <div className='w-80 h-50 text-black rounded-2xl bg-cover bg-center relative flex flex-col justify-end p-5 gap-3 hover:brightness-85 cursor-pointer'
                 style={{ backgroundImage: `url(${props.imgUrl})` }}>
                 <h1 className='z-10 text-xl font-bold bg-white/10 backdrop-blur-md rounded-2xl px-2'>{props.category}</h1>
                 <div className='flex items-center gap-3 bg-white/10 backdrop-blur-md rounded-2xl px-2'>
--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -8,7 +8,7 @@ function PropertyCard(props) {
                 <div className='flex flex-col justify-evenly pl-5 py-2 h-45'>
                     <p className='text-xl font-semibold'>₹ {props.price} <span className='text-gray-600'>/ Month</span> </p>
                     <p>{props.location}</p>
-                    <p>{props.owner} <span className='bg-gray-400 px-1 rounded'>{props.gender}</span></p>
+                    <p>{props.owner} <span className='bg-gray-400 px-1 rounded text-gray-800'>{props.gender}</span></p>
                     <button className='bg-[#D8232A] w-[55%] p-2 text-[18px] cursor-pointer text-white rounded-xl'>Contact Owner</button>
                 </div>
 
